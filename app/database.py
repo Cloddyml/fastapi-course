9c91e151bcf70c363f227123166619f00b91bc65
Couguar <ingluemlp@gmail.com>
2025-12-29 05:00:41 +0300

    Последнее видео из урока 3/14 по PG

--- app/database.py
+++ app/database.py
@@ -3,3 +3,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from app.config import settings
 
 engine = create_async_engine(settings.DB_URL)
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+session = async_session_maker()

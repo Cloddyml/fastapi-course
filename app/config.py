ebc27dd3b08d0f431f56821107090563831bb2b7
Couguar <ingluemlp@gmail.com>
2025-12-29 04:32:52 +0300

    Добавление абсолютного пути для .env файла в config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,17 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class Settings(BaseSettings):
     DB_NAME: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore") # extra позволяет игнорировать переменные окружения, которые есть в .env файле, но отсутсвуют в config.py
-
+    model_config = SettingsConfigDict(
+        # pathlib позволяет формировать путь
+        # с помощью оператора "/", аналогично os.path.join()
+        env_file=BASE_DIR / ".env"
+        # extra="ignore # extra позволяет игнорировать переменные окружения, которые есть в .env файле, но отсутсвуют в config.py
+    )
 
 settings = Settings()

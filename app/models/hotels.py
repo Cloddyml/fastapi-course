f56e2ae43a1a82e35e5e4b2deb6dfdf12b684995
Couguar <ingluemlp@gmail.com>
2026-01-12 14:10:13 +0300

    2 видео, 4/14 Модели и Миграции
    Создание первой миграции Alembic

--- app/models/hotels.py
+++ app/models/hotels.py
@@ -7,5 +7,5 @@ class HotelsOrm(Base):
     __tablename__ = "hotels"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(lenght=100))
+    title: Mapped[str] = mapped_column(String(length=100))
     location: Mapped[str]

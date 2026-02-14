020a8cbe11a751f7cb2ae461762c4f08730a9bfc
Couguar <ingluemlp@gmail.com>
2026-02-14 08:07:50 +0300

    2 видео, 10/14 Модели и Миграции
    Задание на добавление метода GET для получения отеля по ID

--- app/api/hotels.py
+++ app/api/hotels.py
@@ -25,7 +25,13 @@ async def get_hotels(
 
 
 @router.get("/{hotel_id}")
-async def get_hotel(hotel_id: int): ...
+async def get_hotel(
+    hotel_id: int,
+):
+    async with async_session_maker() as session:
+        return await HotelsRepository(session).get_one_or_none(
+            id=hotel_id,
+        )
 
 
 @router.delete("/{hotel_id}")

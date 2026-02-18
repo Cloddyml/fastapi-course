fbd660ba7c68bd02895c832fba2e4bd4961ff4c5
Couguar <ingluemlp@gmail.com>
2026-02-18 11:35:19 +0300

    6 видео, 6/14 База данных: продвинутые запросы, M2M связи и Relationships
    Перенос сырого SQL запроса на язык алхимии для получения свободных номеров по датам, а также добавление фильтрации по отелям

--- app/repositories/rooms.py
+++ app/repositories/rooms.py
@@ -31,7 +31,7 @@ class RoomsRepository(BaseRepository):
             left join rooms_count on rooms.id = rooms_count.room_id
         )
         select * from rooms_left_table
-        where rooms_left > 0;
+        where rooms_left > 0 and room_id in (select id from rooms where hotel_id = 1);
         """
 
         """
@@ -73,13 +73,29 @@ class RoomsRepository(BaseRepository):
 
         """
         select * from rooms_left_table
-        where rooms_left > 0;
+        where rooms_left > 0 and room_id in (select id from rooms where hotel_id = 1);
         """
 
-        query = (
-            select(rooms_left_table)
-            .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0)
+        rooms_ids_for_hotel = (
+            select(RoomsOrm.id)
+            .select_from(RoomsOrm)
+            .filter_by(hotel_id=hotel_id)
+            .subquery(name="rooms_ids_for_hotel")
         )
 
-        print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
+        rooms_ids_to_get = (
+            select(rooms_left_table.c.room_id)
+            .select_from(rooms_left_table)
+            .filter(
+                rooms_left_table.c.rooms_left > 0,
+                rooms_left_table.c.room_id.in_(rooms_ids_for_hotel),
+            )
+        )
+
+        print(
+            rooms_ids_to_get.compile(
+                bind=engine, compile_kwargs={"literal_binds": True}
+            )
+        )
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))

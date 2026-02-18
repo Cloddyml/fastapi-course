8a9b1c2445d5e479d36d168f47c85525e6bd6e54
Couguar <ingluemlp@gmail.com>
2026-02-18 12:35:00 +0300

    1 видео, 7/14 База данных: продвинутые запросы, M2M связи и Relationships
    Правильное решение задания

--- app/repositories/hotels.py
+++ app/repositories/hotels.py
@@ -13,32 +13,6 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(self, location, title, limit, offset) -> list[Hotel]:
-        query = select(HotelsOrm)
-        if title:
-            query = query.filter(
-                func.lower(HotelsOrm.title).contains(title.strip().lower())
-            )
-        if location:
-            query = query.filter(
-                func.lower(HotelsOrm.location).contains(location.strip().lower())
-            )
-
-        query = query.limit(limit).offset(offset)
-
-        # print(query.compile(engine, compile_kwargs={"literal_binds": True}))
-        result = await self.session.execute(query)
-        return [
-            Hotel.model_validate(hotel, from_attributes=True)
-            for hotel in result.scalars().all()
-        ]
-
-        # print(type(result), result)
-        # print(type(hotels), hotels)
-
-        # if pagination.page and pagination.per_page:
-        # return hotels_[pagination.per_page * (pagination.page - 1):][:pagination.per_page]
-
     async def get_filtered_by_time(
         self,
         location,
@@ -47,22 +21,26 @@ class HotelsRepository(BaseRepository):
         offset,
         date_from: date,
         date_to: date,
-    ):
+    ) -> list[Hotel]:
         rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
         hotel_ids_to_get = (
             select(RoomsOrm.hotel_id)
             .select_from(RoomsOrm)
             .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-            .limit(limit)
-            .offset(offset)
         )
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotel_ids_to_get))
         if title:
-            hotel_ids_to_get = hotel_ids_to_get.filter(
+            query = query.filter(
                 func.lower(HotelsOrm.title).contains(title.strip().lower())
             )
         if location:
-            hotel_ids_to_get = hotel_ids_to_get.filter(
+            query = query.filter(
                 func.lower(HotelsOrm.location).contains(location.strip().lower())
             )
+        query = query.limit(limit).offset(offset)
+        result = await self.session.execute(query)
 
-        return await self.get_filtered(HotelsOrm.id.in_(hotel_ids_to_get))
+        return [
+            Hotel.model_validate(hotel, from_attributes=True)
+            for hotel in result.scalars().all()
+        ]

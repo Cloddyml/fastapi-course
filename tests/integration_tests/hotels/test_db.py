7b7d8ebee4e9f57e61686e771156a0c21b232033
Couguar <ingluemlp@gmail.com>
2026-02-20 23:09:31 +0300

    3 видео, 4/13 Тестирование
    Написание теста для добавления бронирования

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -3,6 +3,5 @@ from app.schemas.hotels import HotelAdd
 
 async def test_add_hotel(db):
     hotel_data = HotelAdd(title="Отель 5 звезд", location="Сочи")
-    new_hotel_data = await db.hotels.add(hotel_data)
+    await db.hotels.add(hotel_data)
     await db.commit()
-    print(f"{new_hotel_data=}")

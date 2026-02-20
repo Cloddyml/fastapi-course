b7e5ecbe0345d7118463da51dc39f96a06639214
Couguar <ingluemlp@gmail.com>
2026-02-20 21:53:57 +0300

    1 видео, 3/13 Тестирование
    Регистрация пользователя в тестовую БД через тест

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
+from httpx import ASGITransport, AsyncClient
 
 import app.models  # noqa: F401
 from app.config import settings
 from app.database import Base, engine_null_pool
+from app.main import app
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -11,9 +13,20 @@ async def check_test_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_test_mode):
+async def setup_database(check_test_mode):
     print("Я ФИКСТУРА")
 
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        await ac.post(
+            "/auth/register",
+            json={"email": "kot@pes.com", "password": "1234"},
+        )

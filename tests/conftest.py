56c6458a345f06231a655783f9319223ab34a9ac
Couguar <ingluemlp@gmail.com>
2026-02-20 23:38:18 +0300

    4 видео, 4/13 Тестирование
    Мое решение задания на переписывание теста для бронирования под CRUD

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ async def db() -> DBManager:
 
 @pytest.fixture(scope="session", autouse=True)
 async def setup_database(check_test_mode):
-    print("Я ФИКСТУРА")
+    # print("Я ФИКСТУРА")
 
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
--- tests/integration_tests/booking/test_db.py
+++ tests/integration_tests/booking/test_db.py
@@ -3,7 +3,7 @@ from datetime import date
 from app.schemas.bookings import BookingAdd
 
 
-async def test_add_booking(db):
+async def test_booking_crud(db):
     user_id = (await db.users.get_all())[0].id
     room_id = (await db.rooms.get_all())[0].id
     booking_data = BookingAdd(
@@ -13,5 +13,30 @@ async def test_add_booking(db):
         date_to=date(year=2026, month=1, day=1),
         price=100,
     )
-    await db.bookings.add(booking_data)
+    # CREATE AND READ
+    new_booking = await db.bookings.add(booking_data)
+    check_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert check_booking
+    assert check_booking.id == new_booking.id
+
+    # UPDATE
+    new_date_from = date(year=2026, month=12, day=12)
+    new_date_to = date(year=2027, month=1, day=1)
+    booking_update_data = BookingAdd(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=new_date_from,
+        date_to=new_date_to,
+        price=100,
+    )
+    await db.bookings.edit(booking_update_data)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.date_from == new_date_from
+    assert updated_booking.date_to == new_date_to
+
+    # DELETE
+    await db.bookings.delete(id=updated_booking.id)
+    deleted_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not deleted_booking
     await db.commit()

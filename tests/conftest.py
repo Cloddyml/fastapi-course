6233e1e361bcacf4e70af62ddee041e10e7ec91b
Couguar <ingluemlp@gmail.com>
2026-02-20 21:21:17 +0300

    5 видео, 2/13 Тестирование
    Маленькие изменения
    
    p.s. В предыдущем коммите было видео 4, а не 3

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,13 @@ from app.database import Base, engine_null_pool
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main():
-    print("Я ФИКСТУРА")
+async def check_test_mode():
     assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def async_main(check_test_mode):
+    print("Я ФИКСТУРА")
 
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)

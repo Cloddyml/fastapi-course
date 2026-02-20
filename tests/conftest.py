854fc77dbbdb2ab3d3b591dccda2b1e7239129db
Couguar <ingluemlp@gmail.com>
2026-02-20 22:19:53 +0300

    2 видео, 3/13 Тестирование
    Мое решение задания на добавление при инициализации тестов тестовых значений в БД отелей и номеров

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import json
+
 import pytest
 from httpx import ASGITransport, AsyncClient
 
 import app.models  # noqa: F401
 from app.config import settings
-from app.database import Base, engine_null_pool
+from app.database import Base, async_session_maker_null_pool, engine_null_pool
 from app.main import app
+from app.schemas.hotels import HotelAdd
+from app.schemas.rooms import RoomAdd
+from app.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -30,3 +35,25 @@ async def register_user(setup_database):
             "/auth/register",
             json={"email": "kot@pes.com", "password": "1234"},
         )
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def create_hotels(setup_database):
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        with open("tests/mock_hotels.json", "r") as file:
+            hotels_data = json.load(file)
+        for hotel_data in hotels_data:
+            new_hotel_data = await db.hotels.add(HotelAdd(**hotel_data))
+            await db.commit()
+            print(f"{new_hotel_data=}")
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def create_rooms(setup_database):
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        with open("tests/mock_rooms.json", "r") as file:
+            rooms_data = json.load(file)
+        for room_data in rooms_data:
+            new_room_data = await db.rooms.add(RoomAdd(**room_data))
+            await db.commit()
+            print(f"{new_room_data=}")

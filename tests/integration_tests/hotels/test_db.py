a44d72bd787120beb7f693ac67ee5a3e475cbff4
Couguar <ingluemlp@gmail.com>
2026-02-20 21:00:39 +0300

    3 видео, 2/13 Тестирование
    Постепенное создание асинхронного теста для ручки на добавление отеля
    
    p.s. В предыдущем коммите было видео 2, а не 1

---
+++ tests/integration_tests/hotels/test_db.py
@@ -0,0 +1,11 @@
+from app.database import async_session_maker
+from app.schemas.hotels import HotelAdd
+from app.utils.db_manager import DBManager
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(title="Отель 5 звезд", location="Сочи")
+
+    async with DBManager(session_factory=async_session_maker) as db:
+        new_hotel_data = await db.hotels.add(hotel_data)
+        print(f"{new_hotel_data=}")

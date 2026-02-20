14277ac757c396627efa7b62f22d896aebcf00cb
Couguar <ingluemlp@gmail.com>
2026-02-20 18:51:00 +0300

    1 видео, 3/4 Экосистема Python Backend: Redis и Celery
    Фоновые задачи с использованием FastAPI BackgroundTask

--- app/api/images.py
+++ app/api/images.py
@@ -1,6 +1,6 @@
 import shutil
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, BackgroundTasks, UploadFile
 
 from app.tasks.tasks import resize_image
 
@@ -8,11 +8,12 @@ router = APIRouter(prefix="/images", tags=["Изображение отелей"
 
 
 @router.post("")
-def upload_image(file: UploadFile):
+def upload_image(file: UploadFile, background_task: BackgroundTasks):
     image_path = f"app/static/images/{file.filename}"
     with open(image_path, "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
 
-    resize_image.delay(image_path)
+    # resize_image.delay(image_path)
+    background_task.add_task(resize_image, image_path)
 
     return {"status": "OK"}

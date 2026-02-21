b95baba08c17c882fd40c5a3986fa52a3c4228b3
Couguar <ingluemlp@gmail.com>
2026-02-21 12:54:45 +0300

    3 видео, 5/13 Тестирование
    Мое решение задания на добавление и получение через API удобств

---
+++ tests/integration_tests/facilities/test_api.py
@@ -0,0 +1,17 @@
+async def test_add_facilities(ac):
+    response = await ac.post("/facilities", json={"title": "Wi-Fi"})
+    print(f"{response.json()=}")
+    assert response.status_code == 200
+    response = await ac.post("/facilities", json={"title": "Кондиционер"})
+    print(f"{response.json()=}")
+    assert response.status_code == 200
+
+
+async def test_get_facilities(ac):
+    response = await ac.get(
+        "/facilities",
+        params={},
+    )
+    print(f"{response.json()=}")
+
+    assert response.status_code == 200

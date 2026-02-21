1fca4fefc30e1352287f5cb50051a497a61b925c
Couguar <ingluemlp@gmail.com>
2026-02-21 15:00:20 +0300

    1 видео, 10/13 Тестирование
    Решение задания на бронирование номеров с учетом их количества

--- app/repositories/bookings.py
+++ app/repositories/bookings.py
@@ -5,6 +5,8 @@ from sqlalchemy import select
 from app.models.bookings import BookingsOrm
 from app.repositories.base import BaseRepository
 from app.repositories.mappers.mappers import BookingDataMapper
+from app.repositories.utils import rooms_ids_for_booking
+from app.schemas.bookings import BookingAdd
 
 
 class BookingsRepository(BaseRepository):
@@ -17,3 +19,18 @@ class BookingsRepository(BaseRepository):
         return [
             self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()
         ]
+
+    async def add_booking(self, data: BookingAdd, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=data.date_from,
+            date_to=data.date_to,
+            hotel_id=hotel_id,
+        )
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        rooms_ids_to_book: list[int] = rooms_ids_to_book_res.scalars().all()
+
+        if data.room_id in rooms_ids_to_book:
+            new_booking = await self.add(data)
+            return new_booking
+        else:
+            raise Exception

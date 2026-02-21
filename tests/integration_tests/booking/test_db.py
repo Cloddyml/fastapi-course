1006c7e053335e6999e59de369f152489b25d285
Couguar <ingluemlp@gmail.com>
2026-02-21 11:39:18 +0300

    1 видео, 5/13 Тестирование
    Как можно было решить задание на переписывание теста для бронирования под CRUD

--- tests/integration_tests/booking/test_db.py
+++ tests/integration_tests/booking/test_db.py
@@ -15,23 +15,26 @@ async def test_booking_crud(db):
     )
     # CREATE AND READ
     new_booking = await db.bookings.add(booking_data)
-    check_booking = await db.bookings.get_one_or_none(id=new_booking.id)
-    assert check_booking
-    assert check_booking.id == new_booking.id
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
 
     # UPDATE
     new_date_from = date(year=2026, month=12, day=12)
     new_date_to = date(year=2027, month=1, day=1)
-    booking_update_data = BookingAdd(
+    update_booking_data = BookingAdd(
         user_id=user_id,
         room_id=room_id,
         date_from=new_date_from,
         date_to=new_date_to,
         price=100,
     )
-    await db.bookings.edit(booking_update_data)
+    await db.bookings.edit(update_booking_data, id=new_booking.id)
     updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
     assert updated_booking
+    assert updated_booking.id == new_booking.id
     assert updated_booking.date_from == new_date_from
     assert updated_booking.date_to == new_date_to
 
@@ -39,4 +42,3 @@ async def test_booking_crud(db):
     await db.bookings.delete(id=updated_booking.id)
     deleted_booking = await db.bookings.get_one_or_none(id=new_booking.id)
     assert not deleted_booking
-    await db.commit()
